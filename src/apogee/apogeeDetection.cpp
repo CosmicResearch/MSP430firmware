19af742dcc01fce72f153e0611c32f75729e838c
Hoturan <davidwcorral@hotmail.com>
2018-03-03 11:07:29 +0100

    Minor type changes
    Has not been tested

--- src/apogee/apogeeDetection.cpp
+++ src/apogee/apogeeDetection.cpp
@@ -5,7 +5,7 @@
 
 boolean_t SensApogee::_apogee_pending = true;
 uint8_t SensApogee::_altitude_descending_count = 3;
-int32_t SensApogee::_last_altitude = 0;
+float_t SensApogee::_last_altitude = 0;
 uint8_t SensApogee::_apogee_altitude_delta = 10;
 uint8_t SensApogee::_apogee_delta_count = 5;
 uint8_t SensApogee::_apogee_velocity_counter = 5;
@@ -20,9 +20,9 @@ SensApogee::SensApogee(float_t Q_accel, float_t R_altitude){
 
 boolean_t SensApogee::apogeeDetectionDoubleKF(int32_t altitude, int16_t accel_y, float_t df){
 	kalmanDouble->propagate(accel_y - earth_gravity, df);
-	kalmanDouble->update((float_t) altitude); // very not sure about all this
+	kalmanDouble->update((float_t) altitude / 100); // very not sure about all this
 
-	int32_t new_altitude = (int32_t) kalmanDouble->h;
+	float_t new_altitude = kalmanDouble->h;
 	//*************** VERSION 1 ***************
 	if (new_altitude < _last_altitude) {
 		--_altitude_descending_count;
@@ -34,29 +34,13 @@ boolean_t SensApogee::apogeeDetectionDoubleKF(int32_t altitude, int16_t accel_y,
 	}
 	else {
 		_altitude_descending_count = 5;
-		_last_altitude = kalmanDouble->h;
+		_last_altitude = new_altitude;
 	}
-
-	//*************** VERSION 2 *************** Carefull not to detect launch as apogee
-	/*if (((uint32)(new_altitude - _last_altitude)) < _apogee_altitude_delta){
-		if (_apogee_delta_count > 0) --_apogee_delta_count;
-
-		if (_apogee_pending & (_apogee_delta_count == 0)){
-			_apogee_pending = 0;
-			_last_altitude = new_altitude;
-			return 1;
-		}
-	}
-	else {
-		apogee_delta_count = 5;
-		_last_altitude = kalman.h;
-		return 0;
-	}*/
 	return 0;
 }
 
 boolean_t SensApogee::apogeeDetectionSingleKF(int32_t altitude){
-	int32_t newAlt = kalmanSingle->getFilteredValue(altitude);
+	float_t newAlt = kalmanSingle->getFilteredValue((float_t) altitude / 100);
 
 	if (newAlt < _last_altitude) {
 		--_altitude_descending_count;
@@ -74,15 +58,15 @@ boolean_t SensApogee::apogeeDetectionSingleKF(int32_t altitude){
 }
 
 int32_t SensApogee::getCorrectedAltitudeDoubleKF(){
-	return kalmanDouble->h;
+	return (int32_t) kalmanDouble->h*100;
 }
 
 int32_t SensApogee::getCorrectedAltitudeSingleKF(){
-	return kalmanSingle->getValue();
+	return (int32_t) kalmanSingle->getValue()*100;
 }
 
 
-/*boolean_tean_tean SensApogee::apogeeDetection(int32 accel_y){
+/*boolean_t SensApogee::apogeeDetection(int32 accel_y){
 	kalman.propagate(accel_y - earth_gravity, dt);
 	if (kalman.v < velocityhreshold) {
 		--_apogee_velocity_counter;
