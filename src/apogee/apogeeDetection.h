5c4488dc53c6b5627beec41bc393a71b7182cef7
Unknown <davidwcorral@hotmail.com>
2018-03-05 12:24:42 +0100

    SimpleKalman working
    Must ajust values as it takes too long to predict apogee

--- src/apogee/apogeeDetection.h
+++ src/apogee/apogeeDetection.h
@@ -10,7 +10,7 @@ class SensApogee {
 	private:
 		static boolean_t _apogee_pending; // 1 no apogee dtected, 0 apogee detected
 		static uint8_t _altitude_descending_count;
-		static int32_t _last_altitude;
+		static float_t _last_altitude;
 		static uint8_t _apogee_altitude_delta; // change in altitude
 		static uint8_t _apogee_delta_count;
 		static uint8_t _apogee_velocity_counter;
@@ -18,13 +18,16 @@ class SensApogee {
 		Kalman* kalmanSingle;
 
 	public:
-		SensApogee(float Q_accel, float R_altitude); // Q_accel is the covariance of your accelerometer measurement
-														 // R_altitude is your altitude measurement
+		//Create intance of classes of both Altitude_KF and Kalman in your code
+		//Values used in test are: Altitude_KF K(0.1, 0.1, 0.125);
+		//						   Kalman myF(0.1,1,1023,0);
+		SensApogee(Altitude_KF* K, Kalman* myF);
 		boolean_t apogeeDetectionDoubleKF(int32_t altitude, int16_t accel_y, float_t df); //altitude, vertical acceleration, sampling interval.
 		boolean_t apogeeDetectionSingleKF(int32_t altitude);
 		boolean_t apogeeDetection(int16_t accel_y);
 		int32_t getCorrectedAltitudeDoubleKF();
 		int32_t getCorrectedAltitudeSingleKF();
 		float_t getVelocityDoubleKF();
+		boolean_t apogeeDetection();
 
 };
